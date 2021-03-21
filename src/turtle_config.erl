2b5d1dd397878310dcfde911a4ba34c0a5d4c24f
fulc927 <fulc927@opentelecom.fr>
2021-03-22 00:23:16 +0100

    Update turtle_config.erl

--- src/turtle_config.erl
+++ src/turtle_config.erl
@@ -12,15 +12,22 @@
 
 -spec read_params() -> [map()].
 read_params() ->
-    {ok, Conf} = application:get_env(turtle, connection_config),
+    {ok, Conf} = application:get_env(cctv, connection_config),
     Conf.
 
 -spec conn_params(map()) -> term(). % @todo fix this typespec
-conn_params(Ps) ->
+conn_params(Ps) -> 
+     Ssl_options = application:get_env(cctv, ssl_options),
+        io:format("~p ~n",[Ssl_options]),
+     {_, Ssl_opts} = Ssl_options,
+        io:format("~p ~n",[Ssl_opts]),
     #amqp_params_network {
         username = username(Ps),
         password = password(Ps),
         virtual_host = virtual_host(Ps),
+        %%les paramètres de conf TLS sont passés dans le cctv.app.src
+        %% la configuration certificat se fait dans certs/rmq_server et rmq_client
+        ssl_options = Ssl_opts,
 
         channel_max = maps:get(channel_max, Ps, 0),
         frame_max = maps:get(frame_max, Ps, 0),
@@ -40,7 +47,7 @@ virtual_host(#{ virtual_host := VH }) -> list_to_binary(VH).
 
 -spec validate_conn_name(term()) -> ok | unknown_conn_name.
 validate_conn_name(Name) ->
-    ConfigList = application:get_env(turtle, connection_config, []),
+    ConfigList = application:get_env(cctv, connection_config, []),
     validate_conn_name(Name, ConfigList).
 
 validate_conn_name(_, []) ->
